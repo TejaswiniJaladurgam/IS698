d64b5920183fc9a44b56f4aa180925227a95aeab
Tejaswini Jaladurgam <183238830+TejaswiniJaladurgam@users.noreply.github.com>
2025-02-12 23:59:54 -0500

    create_ec2.py

--- create_ec2.py
+++ create_ec2.py
@@ -5,11 +5,11 @@ ec2 = boto3.resource('ec2', region_name='us-east-1')  # Use your region
 
 # Create an EC2 instance
 instance = ec2.create_instances(
-    ImageId='ami-085ad6ae776d8f09c',  # Your AMI ID
+    ImageId='ami-085xxxxxxxxxc',  # Your AMI ID
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.micro',  # You can change the instance type if needed
-    KeyName='MyWindowsKeyPair',  # Your key pair name
+    KeyName='MyxxxxPair',  # Your key pair name
     TagSpecifications=[
         {
             'ResourceType': 'instance',
