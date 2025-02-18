b1b5ec1f7c353e0e002af0ebc78e93fec3222484
Tejaswini Jaladurgam <183238830+TejaswiniJaladurgam@users.noreply.github.com>
2025-02-17 23:11:23 -0500

    create_bucket.py

--- Lab3/create_bucket.py
+++ Lab3/create_bucket.py
@@ -1,7 +1,7 @@
 import boto3
 
 s3 = boto3.client('s3')
-bucket_name = 'your_name'  # Change this to a globally unique name
+bucket_name = 'unique-bucket-name'  # Change this to a globally unique name
 
 response = s3.create_bucket(Bucket=bucket_name)
 print(f'Bucket {bucket_name} created successfully!')
