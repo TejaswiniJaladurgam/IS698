6215c17c545ed19dc8d8d257edcb5032e30e4d28
Tejaswini Jaladurgam <183238830+TejaswiniJaladurgam@users.noreply.github.com>
2025-02-17 23:16:03 -0500

    delete_bucket.py

--- Lab3/delete_bucket.py
+++ Lab3/delete_bucket.py
@@ -1,7 +1,7 @@
 import boto3
 
 s3 = boto3.client('s3')
-bucket_name = 'my-boto3-s3-bucket-tejaswini'
+bucket_name = 'unique-bucket-name'
 
 s3.delete_bucket(Bucket=bucket_name)
 print(f'Bucket {bucket_name} deleted successfully!')
