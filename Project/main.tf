85e9cc4fcce556deb814ca5015eaf40e81e9d6ab
Tejaswini Jaladurgam <183238830+TejaswiniJaladurgam@users.noreply.github.com>
2025-05-14 19:29:21 -0400

    Add files via upload

---
+++ Project/main.tf
@@ -0,0 +1,55 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+# Create Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "public-subnet"
+  }
+}
+
+# Create Private Subnet
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Create Security Group for EC2 Instances
+resource "aws_security_group" "ec2_sg" {
+  vpc_id = aws_vpc.main.id
+  name        = "ec2-sg"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
